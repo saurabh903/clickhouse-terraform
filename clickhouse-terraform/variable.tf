48fe65c0ede5a0edfbf975d62061c31de01849be
Saurabh Prasad 💪 <saurabhprasad903@gmail.com>
2025-09-13 15:26:28 +0530

    Update variable.tf

--- clickhouse-terraform/variable.tf
+++ clickhouse-terraform/variable.tf
@@ -1,26 +1,44 @@
-variable "region" { default = "us-east-1" }
-variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
 variable "azs" {
-  type    = list(string)
-  default = ["us-east-1a", "us-east-1b"]
+  description = "Availability Zones to use"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "public_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "ami" {
-  description = "AMI ID for Ubuntu"
+  description = "AMI ID for Ubuntu (e.g., latest Ubuntu 22.04 LTS AMI)"
   type        = string
 }
 
 variable "instance_type" {
-  default = "t3.medium"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.medium"
 }
 
 variable "key_name" {
-  description = "SSH keypair name"
+  description = "SSH keypair name to access EC2 instances"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "Your public IP address to allow SSH access"
   type        = string
 }
