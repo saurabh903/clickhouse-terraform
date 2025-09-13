09a591955287cf0742e2905cd8e015ea4017f5be
Saurabh Prasad 💪 <saurabhprasad903@gmail.com>
2025-09-13 15:30:36 +0530

    Update variable.tf

--- clickhouse-terraform/variable.tf
+++ clickhouse-terraform/variable.tf
@@ -1,44 +1,37 @@
 variable "region" {
-  description = "AWS region to deploy resources in"
-  type        = string
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "azs" {
-  description = "Availability Zones to use"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "public_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "ami" {
-  description = "AMI ID for Ubuntu (e.g., latest Ubuntu 22.04 LTS AMI)"
+  description = "AMI ID for Ubuntu"
   type        = string
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.medium"
+  default = "t3.medium"
 }
 
 variable "key_name" {
-  description = "SSH keypair name to access EC2 instances"
+  description = "SSH keypair name stored in Jenkins credentials"
   type        = string
 }
 
 variable "my_ip" {
-  description = "Your public IP address to allow SSH access"
+  description = "Public IP allowed for SSH"
   type        = string
+  default     = ""  # pass via Jenkins environment variable TF_VAR_my_ip
 }
